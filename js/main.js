efffca878388eb018fd89b321ce7303ac06f7331
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-06-12 21:32:25 +0800

    random offset

--- js/main.js
+++ js/main.js
@@ -81,24 +81,43 @@ function MapScene(){
         if (levels[0] == undefined){
           (function(){
             difficulty += 1;
+            var sx = player.attrs.x + 100;  
             switch(difficulty){
             case 1: {
-              var emy1 = {hp: 100, atk: 100, path: 'images/characters/monster-01.png'};
-              levels.push({position: player.attrs.x + 1000, emyAttrs: [emy1]});
-              levels.push({position: player.attrs.x + 2000, emyAttrs: [emy1]});
-              levels.push({position: player.attrs.x + 3000, emyAttrs: [emy1]});
-              levels.push({position: player.attrs.x + 4000, emyAttrs: []});
+              var emys = [
+                {hp: 100, atk: 100, path: 'images/characters/monster-01.png'},
+                {hp: 100, atk: 100, path: 'images/characters/monster-02.png'},
+                {hp: 100, atk: 100, path: 'images/characters/monster-03.png'},
+              ];
+              _.times(10, function(s){
+                sx += 500 + Math.rand(300);
+                levels.push({position: sx, emyAttrs: _.sample(emys, 1)});
+              });
               break;}
             case 2: {
-              levels.push({position: player.attrs.x + 1100, emyAttrs: [{hp: 100, atk: 100, path: 'images/characters/monster-01.png'}]});
-              break;}
-            case 3: {
-              levels.push({position: player.attrs.x + 1100, emyAttrs: [{hp: 100, atk: 100, path: 'images/characters/monster-01.png'}]});
+              var emys = [
+                {hp: 100, atk: 100, path: 'images/characters/monster-01.png'},
+                {hp: 100, atk: 100, path: 'images/characters/monster-02.png'},
+                {hp: 100, atk: 100, path: 'images/characters/monster-03.png'},
+              ];
+              _.times(10, function(s){
+                sx += 500 + Math.rand(300);
+                levels.push({position: sx, emyAttrs: _.sample(emys, 2)});
+              });
               break;}
             default: {
-              levels.push({position: player.attrs.x + 1100, emyAttrs: [{hp: 100, atk: 100, path: 'images/characters/monster-01.png'}]});
+              var emys = [
+                {hp: 100, atk: 100, path: 'images/characters/monster-01.png'},
+                {hp: 100, atk: 100, path: 'images/characters/monster-02.png'},
+                {hp: 100, atk: 100, path: 'images/characters/monster-03.png'},
+              ];
+              _.times(10, function(s){
+                sx += 500 + Math.rand(300);
+                levels.push({position: sx, emyAttrs: _.sample(emys, 3)});
+              });
               break;}
             }
+            levels.push({position: sx + 2000, emyAttrs: []}); //the break time when player change difficulty
           })();
         }
         var level = levels[0];
