00ecce7cb6f4ac611d35d422dde90754f5437677
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-06-13 20:04:34 +0800

    divide into player.js

---
+++ js/player.js
@@ -0,0 +1,54 @@
+function createPlayer(VIEWPORT_X, BASE_Y){
+  var vx = 0, vy = 0;
+  var currentStatus, STATUSES = {};
+  STATUSES.IDLE = new function(){
+    return {
+      updateInput: function(){
+        if (Input.pressed(Input.KEYS.SPACE)) return currentStatus = STATUSES.SING;
+        if (Input.pressed(Input.KEYS.UP) && player.attrs.y == BASE_Y) vy = 15;
+      }
+    };
+  };
+  STATUSES.SING = new function(){
+    function shoot(path){
+      currentStatus = STATUSES.IDLE;
+      sceneManager.getScene().bulletFactory.create(path, {
+        existTime: 100,
+        speed: 20,
+        x: player.attrs.x,
+        y: player.attrs.y,
+        atk: player.attrs.atk,
+        hp: 1
+      });
+    }
+    return {
+      updateInput: function(){
+        if (Input.pressed(Input.KEYS.A)) shoot('images/characters/magic_ball-01.png');
+        if (Input.pressed(Input.KEYS.S)) shoot('images/characters/magic_ball-02.png');
+        if (Input.pressed(Input.KEYS.D)) shoot('images/characters/magic_ball-03.png');
+      }
+    };
+  };
+  currentStatus = STATUSES.IDLE;
+  var player = sceneManager.getScene().characterFactory.create('images/characters/wolf.png', {
+    x: VIEWPORT_X, 
+    y: BASE_Y,
+    hp: 100,
+    atk: 100
+  }, function(){
+    currentStatus.updateInput();
+    if (Input.pressed(Input.KEYS.RIGHT)) vx = 6;
+    else if (Input.pressed(Input.KEYS.LEFT)) vx = 3;
+    else vx = 4;
+    if (player.attrs.y > BASE_Y){
+      vy -= 1; //gravity
+    }
+    player.attrs.x += vx;
+    player.attrs.y += vy;
+    if (player.attrs.y < BASE_Y){
+      player.attrs.y = BASE_Y;
+      vy = 0;
+    }
+  });
+  return player;
+}
